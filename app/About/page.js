df54e964584d36b7e05c2381fcc9d4ab9686604a
Diego <diegofreeman78@gmail.com>
2023-05-15 15:35:14 +0100

    another commit

--- app/About/page.js
+++ app/About/page.js
@@ -44,11 +44,11 @@ const About = () => {
               Biography
             </h2>
             <p className="font-medium">
-              Hi, I'm CodeBucks, a web developer and UI/UX designer with a
-              passion for creating beautiful, functional, and user-centered
-              digital experiences. With 4 years of experience in the field. I am
-              always looking for new and innovative ways to bring my clients'
-              visions to life.
+              Hi, I'm Diego Freeman, a web and mobile developer with a passion
+              for creating beautiful, functional, and user-centered digital
+              experiences. With 2 years of experience in the field. I am always
+              looking for new and innovative ways to bring my clients' visions
+              to life.
             </p>
             <p className="font-medium my-4">
               I believe that design is about more than just making things look
